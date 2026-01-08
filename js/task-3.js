494c2e848ba0825c9fcd9c78dc0ba375735b4ecd
Liliia <plv-86@ukr.net>
2026-01-08 21:23:41 +0200

    commit message

---
+++ js/task-3.js
@@ -0,0 +1,15 @@
+function filterArray(numbers, value) {
+    const newNumber = [];
+    for (const element of numbers) {
+        if (element > value) {
+            newNumber.push(element);
+        }
+    }
+    return newNumber;
+}
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
